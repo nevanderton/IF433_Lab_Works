99b0c6aa2ed14a1d603603a70ea12020464baa44
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-04 18:12:46 +0700

    week01: add calculateStatus function

--- src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
+++ src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
@@ -1,13 +1,14 @@
 package oop_00000106618_NevanAndertonLie.week01 // Pastikan package sesuai
 
+fun calculateStatus(score: Int) =
+    if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name: String = "John Thor"
     val score: Int = 80
 
-    // Concatenation gaya lama
     println("Nama: " + name + ", Nilai: " + score)
 
-    // Tambahkan di bawah println
     val grade = when (score) {
         in 90..<=100 -> "A"
         in 80..<=89 -> "B"
@@ -16,4 +17,6 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
