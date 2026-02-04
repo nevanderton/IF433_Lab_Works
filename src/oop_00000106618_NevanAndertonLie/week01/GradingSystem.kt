fb4dabee23c93241c51a8c126ffba17aea72842b
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-04 18:11:57 +0700

    week01: implement grading logic using when expression

--- src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
+++ src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
@@ -6,4 +6,14 @@ fun main() {
 
     // Concatenation gaya lama
     println("Nama: " + name + ", Nilai: " + score)
+
+    // Tambahkan di bawah println
+    val grade = when (score) {
+        in 90..<=100 -> "A"
+        in 80..<=89 -> "B"
+        in 70..<=79 -> "C"
+        else -> "D"
+    }
+
+    println("Grade kamu: $grade")
 }
