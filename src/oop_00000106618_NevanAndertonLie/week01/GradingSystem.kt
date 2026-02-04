1151e346c905d0c779c72e2cb3d3719f676e4f04
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-04 18:11:07 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000106618_NevanAndertonLie.week01 // Pastikan package sesuai
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
