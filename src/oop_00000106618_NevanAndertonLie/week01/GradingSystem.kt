d1a61beb07ec25a6bb0aa54bd73c6c3de3a2df75
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-04 18:11:24 +0700

    week01: refactor to use val and string templates

--- src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
+++ src/oop_00000106618_NevanAndertonLie/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_00000106618_NevanAndertonLie.week01 // Pastikan package sesuai
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
     // Concatenation gaya lama
     println("Nama: " + name + ", Nilai: " + score)
