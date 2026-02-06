4bc5639e4aad6d5db3af1c37eca34b819ea4e373
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-06 19:24:57 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000106618_NevanAndertonLie/week01/GameStore.kt
+++ src/oop_00000106618_NevanAndertonLie/week01/GameStore.kt
@@ -4,4 +4,16 @@ fun main() {
 
     val gameTitle = "Elden Ring"
     val price = 750_000
+
+    val finalPrice = calculateDiscount(price)
+
+    println("Judul Game : $gameTitle")
+    println("Harga Asli : Rp$price")
+    println("Harga Akhir: Rp$finalPrice")
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000)
+        price - (price * 20 / 100)
+    else
+        price - (price * 10 / 100)
