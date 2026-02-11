5498e93b7f3e4cb823ca45760a57f2a50a0f1495
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-11 20:44:58 +0700

    week02: create main function and instantiate Student object

---
+++ src/oop_00000106618_NevanAndertonLie/week02/Main.kt
@@ -0,0 +1,15 @@
+package oop_00000106618_NevanAndertonLie.week02
+
+fun main() {
+
+    val student1 = Student(
+        nim = "0000010661",
+        name = "Nevan Anderton Lie",
+        major = "Informatics"
+    )
+
+    println("=== STUDENT DATA ===")
+    println("NIM   : ${student1.nim}")
+    println("Name  : ${student1.name}")
+    println("Major : ${student1.major}")
+}
