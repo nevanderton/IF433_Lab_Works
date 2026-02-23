94d68bf2887b6b48eccaafaf13b442350429786b
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-23 17:10:05 +0700

    week04: override honk method in Car

--- src/oop_00000106618_NevanAndertonLie/week04/Car.kt
+++ src/oop_00000106618_NevanAndertonLie/week04/Car.kt
@@ -5,6 +5,10 @@ class Car(
     val numberOfDoors: Int
 ) : Vehicle(brand) {
 
+    override fun honk() {
+        println("$brand membunyikan klakson mobil: Tiiin! Tiiin!")
+    }
+
     fun openTrunk() {
         println("$brand membuka bagasi.")
     }
