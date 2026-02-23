0612966252e0d0a0a23b2d33fcb1d111eb9effca
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-23 18:14:12 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000106618_NevanAndertonLie/week03/Player.kt
@@ -0,0 +1,21 @@
+package oop_00000106618_NevanAndertonLie.week03
+
+class Player(val username: String) {
+
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) return
+
+        val oldLevel = level
+        xp += amount
+        val newLevel = level
+
+        if (newLevel > oldLevel) {
+            println("Level Up! Selamat $username naik ke level $newLevel")
+        }
+    }
+}
