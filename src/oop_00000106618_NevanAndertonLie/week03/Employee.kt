2fd3230b6867e2259e9014fe076e7b60c2ce65f0
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-23 17:48:10 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000106618_NevanAndertonLie/week03/Employee.kt
+++ src/oop_00000106618_NevanAndertonLie/week03/Employee.kt
@@ -1,6 +1,10 @@
 package oop_00000106618_NevanAndertonLie.week03
 
+
 class Employee {
-    var name: String = ""
+
     var salary: Int = 0
+        set(value) {
+            this.salary = value   // SALAH -> menyebabkan infinite recursion
+        }
 }
