34a323c3c8507a1eaf4ee85546ee66c04319a63c
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-23 18:10:04 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000106618_NevanAndertonLie/week03/Weapon.kt
@@ -0,0 +1,26 @@
+package oop_00000106618_NevanAndertonLie.week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("Damage tidak boleh negatif!")
+                }
+                value > 1000 -> {
+                    field = 1000
+                }
+                else -> {
+                    field = value
+                }
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
