ad97f33882bd38fa03e87052904cb179d3044cd8
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-23 17:14:47 +0700

    week04: create Electric abstract class

---
+++ src/oop_00000106618_NevanAndertonLie/week04/Electric.kt
@@ -0,0 +1,10 @@
+package oop_00000106618_NevanAndertonLie.week04
+
+abstract class Electric {
+
+    abstract fun chargeBattery()
+
+    fun showChargingPort() {
+        println("Menampilkan port pengisian baterai.")
+    }
+}
