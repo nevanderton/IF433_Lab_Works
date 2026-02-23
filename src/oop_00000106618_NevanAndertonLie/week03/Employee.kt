90b2c4ec371e8c7a8139d57ab86bb576ab6428b8
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-02-23 18:06:38 +0700

    week03: add private property

--- src/oop_00000106618_NevanAndertonLie/week03/Employee.kt
+++ src/oop_00000106618_NevanAndertonLie/week03/Employee.kt
@@ -2,6 +2,8 @@ package oop_00000106618_NevanAndertonLie.week03
 
 class Employee {
 
+    var name: String = ""
+
     var salary: Int = 0
         set(value) {
             if (value >= 0) {
@@ -10,4 +12,16 @@ class Employee {
                 println("Salary tidak boleh negatif!")
             }
         }
+
+    // Hanya bisa diakses di dalam class
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
