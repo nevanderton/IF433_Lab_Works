683bfe5f5912a9bf829d91c4280c389da762f577
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-04 11:11:38 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000106618_NevanAndertonLie/week05/Main.kt
+++ src/oop_00000106618_NevanAndertonLie/week05/Main.kt
@@ -56,3 +56,12 @@ fun main() {
     for (metode in metodePembayaran) {
 
         metode.processPayment(75000.0)
+
+        if (metode is EWallet) {
+            metode.topUp(50000.0)
+            metode.processPayment(75000.0)
+        }
+
+        println("----------------------------")
+    }
+}
