6470f60b344aa3025dbd1b9c301bbc8652893893
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-04 10:53:57 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_00000106618_NevanAndertonLie/week05/Dosen.kt
@@ -0,0 +1,13 @@
+package oop_00000106618_NevanAndertonLie.week05
+
+
+class Dosen(nama: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("Dosen $nama sedang mengajar di kelas.")
+    }
+
+    fun mengajar() {
+        println("Dosen $nama memberikan materi perkuliahan.")
+    }
+}
