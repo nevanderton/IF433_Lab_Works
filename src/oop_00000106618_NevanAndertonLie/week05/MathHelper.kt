3a55cdb1efcd82725009b3fba5902c00a17133d3
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-04 11:01:55 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000106618_NevanAndertonLie/week05/MathHelper.kt
@@ -0,0 +1,16 @@
+package oop_00000106618_NevanAndertonLie.week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
