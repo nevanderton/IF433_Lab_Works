3d8cace042299d720582dc7240dad7df6ca3f23e
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-04 10:57:14 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_00000106618_NevanAndertonLie/week05/Admin.kt
@@ -0,0 +1,12 @@
+package oop_00000106618_NevanAndertonLie.week05
+
+class Admin(nama: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("$nama sedang duduk di depan komputer melayani administrasi.");
+    }
+
+    fun doAdmingWork() {
+        println("$nama sedang merekap data absensi mahasiswa");
+    }
+}
