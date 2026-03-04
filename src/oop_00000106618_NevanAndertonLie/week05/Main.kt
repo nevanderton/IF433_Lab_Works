f4ac6724701619070543fdc4f5e035f34b06d903
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-04 11:11:16 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000106618_NevanAndertonLie/week05/Main.kt
+++ src/oop_00000106618_NevanAndertonLie/week05/Main.kt
@@ -45,4 +45,14 @@ fun main() {
 
     val luasLingkaran = mathHelper.hitungLuas(7.0)
     println("Luas Lingkaran: $luasLingkaran")
-}
+
+    println("=== SISTEM PEMBAYARAN ===")
+
+    val eWallet = EWallet("Nevan", 50000.0)
+    val creditCard = CreditCard("Nevan", 100000.0)
+
+    val metodePembayaran: List<PaymentMethod> = listOf(eWallet, creditCard)
+
+    for (metode in metodePembayaran) {
+
+        metode.processPayment(75000.0)
