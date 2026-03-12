5170ee7005061a35afea43c40ecce3eb447fdbc6
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-12 13:48:51 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000106618_NevanAndertonLie/week06/Main.kt
+++ src/oop_00000106618_NevanAndertonLie/week06/Main.kt
@@ -2,10 +2,11 @@ package oop_00000106618_NevanAndertonLie.week06
 
 fun processCheckout(method: PaymentMethod, amount: Double) {
     println("-> Memulai checkout...")
-    method.pay(amount) // Dynamic polymorphism in action
+    method.pay(amount)
 }
 
 fun main() {
+
     val myWatch = Smartwatch()
     myWatch.showTime()
 
@@ -19,7 +20,17 @@ fun main() {
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
 
+    println("\n=== SMART HOME SYSTEM ===")
+
     val lamp = SmartLamp(id = "L001", name = "Ruang Tamu")
     val speaker = SmartSpeaker(id = "S001", name = "Google Nest Dapur")
     val cctv = SmartCCTV(id = "C001", name = "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
