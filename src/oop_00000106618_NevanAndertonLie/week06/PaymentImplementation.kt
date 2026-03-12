c07666932b0568c4c64d18d1c713439fb36a7f1c
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-03-12 13:44:21 +0700

    week06: create Gopay and CreditCard

---
+++ src/oop_00000106618_NevanAndertonLie/week06/PaymentImplementation.kt
@@ -0,0 +1,13 @@
+package oop_00000106618_NevanAndertonLie.week06
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Contacting Bank for Rp$amount")
+    }
+}
