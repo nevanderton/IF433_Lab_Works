3a4a3f0d393861f95a7b19797d77b7b86f68ddf3
Nevan Anderton Lie <hiddenkeren@gmail.com>
2026-05-10 15:39:20 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000106618_NevanAndertonLie/week07/Main.kt
+++ src/oop_00000106618_NevanAndertonLie/week07/Main.kt
@@ -12,12 +12,18 @@ fun main() {
     println("\n=== TEST REGULAR CLASS ===")
     val reg1 = RegularUser("Alice", 22)
     val reg2 = RegularUser("Alice", 22)
-    println(reg1) // Akan mencetak memori hash
-    println("Sama? ${reg1 == reg2}") // False
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
 
     println("\n=== TEST DATA CLASS ===")
     val data1 = DataUser("Alice", 22)
     val data2 = DataUser("Alice", 22)
-    println(data1) // Otomatis readable format
-    println("Sama? ${data1 == data2}") // True (Structural Equality)
+    println(data1)
+    println("Sama? ${data1 == data2}")
+
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    val (userName, userAge) = data1 // Destructuring Declaration
+    println("Destructured: $userName berumur $userAge")
 }
